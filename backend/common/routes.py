5504cf5e2ae6a9e4fa0fd615ac45d2bfcc81cef5
Philip Lam <philiv.lam@gmail.com>
2025-09-30 13:03:40 +0200

    chore(#19): Change setup for counter endpoint so it appears in schema.yml

--- backend/common/routes.py
+++ backend/common/routes.py
@@ -1,6 +1,7 @@
-from .views import RestViewSet
+from .views import CounterViewSet, RestViewSet
 
 
 routes = [
     {"regex": r"rest", "viewset": RestViewSet, "basename": "Rest"},
+    {"regex": r"counter", "viewset": CounterViewSet, "basename": "counter"}
 ]
