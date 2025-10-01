e653fccf8db6eb52196eae189f3eb578ba3ec0f8
Adrian Sandes Lillevoll <adriasl.stud.ntnu.no>
2025-10-01 13:46:29 +0200

    button(#5-1): added send-button under search/text -field

--- frontend/js/pages/Home.tsx
+++ frontend/js/pages/Home.tsx
@@ -2,6 +2,7 @@ import { SidebarProvider, SidebarTrigger } from "../components/ui/sidebar";
 import { AppSidebar } from "../components/ui/app-sidebar";
 import { Textarea } from "../components/ui/textarea"
 import { FourButtons } from "../components/ui/app-four-buttons";
+import { Button } from "../components/ui/button";
 
 const Home = () => {
 
@@ -14,8 +15,9 @@ const Home = () => {
           <div className="fixed top-70 left-120 right-56 mb-6">
             <FourButtons />
           </div>
-          <div className="fixed bottom-20 left-120 right-56">
+          <div className="grid gap-2 fixed bottom-20 left-120 right-56">
             <Textarea placeholder="Skriv noe her..." />
+            <Button>Send</Button>
           </div>
         </main>
       </SidebarProvider>
