ab6ba3ede18b3b585e08b00047f2b459de7804ac
Jespese <jespese@stud.ntnu.no>
2025-10-07 16:55:20 +0200

    chore(#7): create stylesheet for trondheim kommunes graphical profile

--- frontend/js/index.tsx
+++ frontend/js/index.tsx
@@ -3,12 +3,12 @@ import * as Sentry from "@sentry/browser";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-
+import "../css/tkr-theme.css";
 import "../css/style.css";
 
 Sentry.init({
-  dsn: window.SENTRY_DSN,
-  release: window.COMMIT_SHA,
+	dsn: window.SENTRY_DSN,
+	release: window.COMMIT_SHA,
 });
 
 const root = createRoot(document.getElementById("react-app") as HTMLElement);
