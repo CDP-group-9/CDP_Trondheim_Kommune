aae5fd49e4f08d2e827f505e2decb705244c96f4
Elin Skaanlund Berntsen <elinsberntsen@gmail.com>
2025-10-10 12:01:12 +0200

    feat(#46): add progress bar on top of relevant pages

---
+++ frontend/js/components/ui/progressbar-updated.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+import { Progress } from "./progress";
+
+const ProgressBarUpdated = () => {
+  const [progress, setProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const docHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const scrollProgress = (scrollTop / docHeight) * 100;
+      setProgress(scrollProgress);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="fixed top-0 left-0 w-full z-50 bg-transparent">
+      <Progress className="h-2 w-full rounded-none" value={progress} />
+    </div>
+  );
+};
+
+export default ProgressBarUpdated;
