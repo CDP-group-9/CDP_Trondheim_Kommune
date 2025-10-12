22fc854f2011846a269956dac55bcef9983d5498
Adrian Sandes Lillevoll <adriasl.stud.ntnu.no>
2025-10-12 11:53:38 +0200

    Reapply "feat(#46): add progress bar on top of relevant pages"
    This reverts commit 634d82a74cbc55ccc089372085e5addfd706eb53.

--- frontend/js/pages/Privacy.tsx
+++ frontend/js/pages/Privacy.tsx
@@ -5,10 +5,12 @@ import {
   KeyPrinciplesSection,
   ContactInfoSection,
 } from "../components/ui/privacy-sections";
+import ProgressBarUpdated from "../components/ui/progressbar-updated";
 
 const Privacy = () => {
   return (
     <div className="flex justify-center mb-4">
+      <ProgressBarUpdated />
       <div className="flex-1 space-y-6 max-w-4xl px-6 py-4">
         <div className="text-center space-y-4">
           <h1 className="text-3xl font-medium mb-4">
