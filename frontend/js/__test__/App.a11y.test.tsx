040267f1c5737c2e41eeed9612fb445921ca8a0a
Jespese <jespese@stud.ntnu.no>
2025-10-22 13:16:03 +0200

    refactor[#113]: move App accessability test into a test folder

--- frontend/js/__test__/App.a11y.test.tsx
+++ frontend/js/__test__/App.a11y.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import { axe } from "jest-axe";
 
-import App from "./App";
+import App from "../App";
 
 describe("App accessibility", () => {
   test("has no basic accessibility violations", async () => {
